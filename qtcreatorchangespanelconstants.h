d4fb96e40d6253c16e8d9db12ba8a9f114c27869
Petr Mironychev <9195189+Palm1r@users.noreply.github.com>
2026-07-03 20:56:55 +0200

    feat: Init plugin

---
+++ qtcreatorchangespanelconstants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace QtCreatorChangesPanel::Constants {
+
+const char ACTION_ID[] = "QtCreatorChangesPanel.Action";
+const char MENU_ID[] = "QtCreatorChangesPanel.Menu";
+
+} // namespace QtCreatorChangesPanel::Constants
---
+++ qtcreatorchangespaneltr.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <QCoreApplication>
+
+namespace QtCreatorChangesPanel {
+
+struct Tr
+{
+    Q_DECLARE_TR_FUNCTIONS(QtC::QtCreatorChangesPanel)
+};
+
+} // namespace QtCreatorChangesPanel
